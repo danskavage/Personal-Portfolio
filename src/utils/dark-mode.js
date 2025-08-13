fee8d91d6fabd092ee8334c3abb9b20bda006dca
danskavage <ddaniel4582@gmail.com>
2025-08-13 14:00:23 +0300

    Initial commit

---
+++ src/utils/dark-mode.js
@@ -0,0 +1,22 @@
+const darkMode = () => {
+    
+    const themeToggleBts = document.querySelectorAll("#theme-toggle");
+    const theme = localStorage.getItem('theme');
+    theme && document.body.classList.add(theme);
+
+    //handler
+    const handleThemeToggle = () => {
+        document.body.classList.toggle('light-mode'); 
+            if (document.body.classList.contains('light-mode')) {
+                localStorage.setItem('theme', 'light-mode');
+            } else {
+                localStorage.removeItem('theme');
+                document.body.removeAttribute('class');
+            }
+    };
+    themeToggleBts.forEach(btn => 
+        btn.addEventListener('click', handleThemeToggle)
+    ); 
+};
+
+export default darkMode;
